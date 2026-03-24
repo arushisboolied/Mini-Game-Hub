95a03576deeb6aedc7c8e339d1a0c2f728c4eab0
Arush Anand <arushanand107@gmail.com>
2026-03-24 23:04:05 +0530

    Update main.sh
    Added functions to fetch hashed pswd and register users

--- hub/main.sh
+++ hub/main.sh
@@ -9,3 +9,17 @@ hashing_passwords(){
 user_existence(){
     cut -f1 "users.tsv" | grep -qx "$1"
 }
+
+#fetch hashed password
+fetch_hash(){
+    grep "^$1|" "users.tsv" | cut -d "|" -f2
+}
+
+#registering users
+register_user(){
+    local username="$1"
+    local password="$2"
+    local hashed_password=$(hashing_passwords "$password")
+    echo "$username|$hashed_password" >> "users.tsv"
+    echo "User '$username' has beenregistered successfully."
+}
