e77042743f678c031557a7a034c9f6b850c440dd
Arush Anand <arushanand107@gmail.com>
2026-04-29 20:35:08 +0530

    Added comments to Menu.py

--- hub/Menu.py
+++ hub/Menu.py
@@ -11,30 +11,34 @@ from tictactoe import TicTacToe
 from othello import Othello
 from connect4 import Connect4
 
+##### Takes in Users as arguements #####
 User1=sys.argv[1]
 User2=sys.argv[2]
 
 class Menu:
+
+    ##### Setting up a resizable pygame display #####
     def __init__(self,Resolution=(1280,720)):
         pygame.init()
         pygame.font.init()
         self.Resolution=Resolution
         self.character=0
-        self.screen=pygame.display.set_mode(Resolution,pygame.RESIZABLE)
-        self.clock=pygame.time.Clock()
-        self.level=Level()
-        
-
+        self.screen=pygame.display.set_mode(Resolution,pygame.RESIZABLE)        # Resizability
+        self.clock=pygame.time.Clock()                                          # Controls frame rate
+        self.level=Level()                                                      # Loads the menu level (UI, Player, Settings etc.)
+    
+    ##### Handle Keyboard Inputs - E to interact #####
     def event_handler(self,event):
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_e and self.level.player.check_zones():
                 self.level.settings_toggle()
 
+    ##### Main Game Loop #####
     def run(self):
         running=True
         
         while running:
-            self.clock.tick(60)
+            self.clock.tick(60) #60FPS 
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     running=False
@@ -43,33 +47,47 @@ class Menu:
                 else:
                     self.event_handler(event)
 
+            ##### Clear Screen #####
             self.screen.fill("black")
-            self.level.run()
-            if self.level.settings.current_game!=None:
 
+            ##### Run the current menu level #####
+            self.level.run()
+
+            ##### To check if a game has been selected #####
+            if self.level.settings.current_game!=None:
+                
+                ##### Launch TicTacToe #####
                 if self.level.settings.current_game=="Tictactoe":
                     TicTacToe(theme=Theme_mapping[self.level.game_theme],Characters=(self.level.User1_selection,self.level.User2_selection),players=(User1,User2)).run()
 
+                ##### Launch Connect4 #####
                 elif self.level.settings.current_game=="Connect4":
                     Connect4(theme=Theme_mapping[self.level.game_theme],Characters=(self.level.User1_selection,self.level.User2_selection),players=(User1,User2)).run()
 
+                ##### Launch Othello #####
                 elif self.level.settings.current_game=="Othello":
                     Othello(theme=Theme_mapping[self.level.game_theme],Characters=(self.level.User1_selection,self.level.User2_selection),players=(User1,User2)).run()
 
+                ##### Leaderboard (After game ends) #####
                 elif self.level.settings.current_game=="Leaderboard":
                     print("Leaderboard called")
+                
+                ##### Exit Game #####
                 elif self.level.settings.current_game=="Exit":
                     running=False
                     pygame.quit()
                     sys.exit()
 
+                ##### After game ends, reset selections #####
                 self.level.settings.current_game=None
                 self.level.game_paused=False
 
+            ##### Independent trigger for leaderboard #####
             if self.level.call_leaderboard:
                 print("Leaderboard called")
                 self.level.call_leaderboard=False
             pygame.display.update()
             
 
-Menu().run()
+##### Enjoy :) #####
+Menu().run() 
